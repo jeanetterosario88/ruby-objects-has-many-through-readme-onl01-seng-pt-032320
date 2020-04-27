f8f5e1121d3707f1f553155a88ba84f468d3cb21
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-27 23:50:38 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,5 +23,10 @@ class Waiter
     meal.waiter == self #checking for waiter now
     end
   end
+  
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
  
 end
